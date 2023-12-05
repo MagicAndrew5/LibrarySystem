a5a8a574a4c4886a147edc0bddbc3cf03f14c5ed
Andrea Messa <andrea.messa@usi.ch>
2023-12-05 23:56:30 +0100

    First Commit

---
+++ src/main/java/com/unimib/lybrarysystem/LybrarySystemApplication.java
@@ -0,0 +1,13 @@
+package com.unimib.lybrarysystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LybrarySystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LybrarySystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/unimib/lybrarysystem/LybrarySystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unimib.lybrarysystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LybrarySystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
